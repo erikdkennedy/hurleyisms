639d4acfaa2c3cbc4557db7053a4b606d612e54f
Andrew <LimeyJohnson@gmail.com>
2016-04-16 08:14:39 -0700

    Working on angular setup

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,7 +1,9 @@
-angular.module('hurleyisms', [])
+angular.module('hurleyisms', [''])
   .controller('AdminController', ['$scope', '$http', function($scope, $http) {
     $scope.Lines = [];
     $http.get('admin/data').then(function(data){
     	cachedLines = data;
     });
+    console.log("got here");
+    $scope.test = "got it";
   }]);
