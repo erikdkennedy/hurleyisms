6ff0b0267c7e5f08ad107d15bff1149295b759e7
Andrew <LimeyJohnson@gmail.com>
2016-06-29 07:37:59 -0700

    Added command to remove all users. Working on getting auth with cookie going.

--- routes/auth.js
+++ routes/auth.js
@@ -32,15 +32,17 @@ router.post('/register', function (req, res) {
             sendJSONresponse(res, 404, err);
         } else {
             token = user.generateJwt();
+            res.cookie('auth', token, { path: '/', httpOnly: true, maxAge: 604800000 });
             sendJSONresponse(res, 200, {
-                "token" : token
             });
+            return next();
         }
     });
 
 });
 
 router.post('/login', function(req, res) {
+    console.log("Login Called");
     if(!req.body.email || !req.body.password) {
         sendJSONresponse(res, 400, {
             "message": "All fields required"
@@ -57,14 +59,13 @@ router.post('/login', function(req, res) {
         }
 
         if(user){
-            token = user.generateJwt();
+            res.cookie('auth', token, { path: '/', httpOnly: true, maxAge: 604800000 });
             sendJSONresponse(res, 200, {
-                "token" : token
             });
         } else {
             sendJSONresponse(res, 401, info);
         }
-    })(req, res);
 
+    });
 });
 module.exports = router;
