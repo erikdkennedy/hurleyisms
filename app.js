d69002cf91a17e47571cda79bdb883b1fefbd4f2
Andrew Johnson <LimeyJohnson@gmail.com>
2016-06-29 08:04:24 -0700

    Backend auth now working

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 require('./models/db');
+require('./config/passport');
 var routes = require('./routes/index');
 var admin = require('./routes/admin');
 var users = require('./routes/users');
