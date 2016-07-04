016602fbac81658662187964bfbd30acf0f27fae
Andrew Johnson <LimeyJohnson@gmail.com>
2016-07-04 10:18:55 -0700

    Had to change index.js to main.js so that express static wouldn't serve the page without calling the router first. When a user is logged in they automatically go to the app page.

---
+++ routes/helpers.js
@@ -0,0 +1,43 @@
+﻿function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/auth/login');
+}
+function isLoggedIn(req){
+    return req.payload;
+}
+function ensureAdmin(req, res, next) {
+    if (req.isAuthenticated() && req.user.admin) {
+        return next();
+    }
+    res.redirect('/auth/login');
+}
+
+function ensureAdminJSON(req, res, next) {
+    if (req.isAuthenticated() && req.user.admin) {
+        return next();
+    }
+    res.status(401)
+    .json({
+        status: 'error',
+        message: 'You do not have permission to do that.'
+    });
+}
+
+function loginRedirect(req, res, next) {
+    if (req.isAuthenticated()) {
+        res.redirect('/');
+    } else {
+        return next();
+    }
+}
+
+
+module.exports = {
+    ensureAuthenticated: ensureAuthenticated,
+    ensureAdmin: ensureAdmin,
+    ensureAdminJSON: ensureAdminJSON,
+    loginRedirect: loginRedirect,
+    isLoggedIn: isLoggedIn
+};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var helpers = require('./helpers');
 /* GET home page. */
 
 
-router.get('/', function(req, res, next) {
-    res.sendFile('public/index.html');
+router.get('/', function (req, res, next) {
+    console.log("getting index");
+    if (helpers.isLoggedIn(req)) {
+        res.redirect("app");
+    }
+    else {
+        res.sendFile(path.join(__dirname, '../public', 'main.html'));
+    }
 });
 
 router.get('/pro', function (req, res, next) {
