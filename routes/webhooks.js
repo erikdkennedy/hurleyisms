9328157f516dbaf82d8b70f535922254994d65d5
Andrew Johnson <LimeyJohnson@gmail.com>
2016-07-08 07:54:33 -0700

    Verifying we are not getting an error from stripe

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -7,14 +7,24 @@ router.post("/", function (req, res) {
     console.log(req.body);
     if (req.body && req.body.id) {
         stripe.events.retrieve(req.body.id, function (err, event_recv) {
-            var event = {};
-            event.id = event_recv.id;
-            event.event = event_recv;
-            events.create(event, function (err, result) {
-                console.log(err);
-                res.sendStatus(200);
-            });
+            if (err) {
+                console.error(err);
+                res.sendStatus(404);
+            }
+            else {
+                var event = {};
+                event.id = event_recv.id;
+                event.event = event_recv;
+                events.create(event, function (err, result) {
+                    console.log(err);
+                    res.sendStatus(200);
+                });
+            }
         });
+    }
+    else
+    {
+        res.sendStatus(400);
     }
 });
 module.exports = router;
