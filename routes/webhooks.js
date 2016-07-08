5b9ed8107a3cc650e475fd888bf7010e41d5b169
Andrew Johnson <LimeyJohnson@gmail.com>
2016-07-08 08:21:12 -0700

    Removed verifying stripe webhook

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -5,22 +5,24 @@ var events = mongoose.model('events');
 var stripe = require("stripe")(process.env.STRIPE_KEY);
 router.post("/", function (req, res) {
     console.log(req.body);
+    //Stripes events api was not returning the event correctly
+    //So event verification is currently turned off.
     if (req.body && req.body.id) {
-        stripe.events.retrieve(req.body.id, function (err, event_recv) {
-            if (err) {
-                console.error(err);
-                res.sendStatus(404);
-            }
-            else {
+      //  stripe.events.retrieve(req.body.id, function (err, event_recv) {
+        //    if (err) {
+          //      console.error(err);
+         //       res.sendStatus(404);
+         //   }
+        //    else {
                 var event = {};
-                event.id = event_recv.id;
-                event.event = event_recv;
+                event.id = req.body.id;
+                event.event = req.body;
                 events.create(event, function (err, result) {
                     console.log(err);
                     res.sendStatus(200);
                 });
-            }
-        });
+            //}
+      //  });
     }
     else
     {
