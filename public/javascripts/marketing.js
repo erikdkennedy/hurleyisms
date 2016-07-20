c9dbd2612aa1cc5f12af32bda57c6415590961fd
Erik Kennedy <erik.d.kennedy@gmail.com>
2016-07-20 09:24:35 -0700

    marketing page fixes for 4k monitors

--- public/javascripts/marketing.js
+++ public/javascripts/marketing.js
@@ -1,15 +1,43 @@
 $(document).ready(function() {
 
-	$(window).scroll(function() {
-		//TODO don't show animations on mobile
+	function triggerScrollingAnimations() {
+
+		if (isMobile()) {
+			removeScrollListener();
+			$(".card").addClass("flipped");
+			$(".device-img").addClass("slide-in");
+			return false;
+		}
+
+		var animationsComplete = 0;
+		var totalAnimations = 2;
+
 		//TODO cache offsets
-		//TODO remove listener after it's triggered
 		if ($(window).scrollTop() + 0.35*$(window).height() > $("#card-1").offset().top) {
 			$(".card").addClass("flipped");
+			animationsComplete++;
 		}
 
 		if ($(window).scrollTop() + 0.75*$(window).height() > $("#device-img-1").offset().top) {
 			$(".device-img").addClass("slide-in");
+			animationsComplete++;
 		}
+
+		if (animationsComplete === totalAnimations) removeScrollListener();;
+	}
+
+	function isMobile() {
+		return window.matchMedia("(max-width: 568px)").matches;
+	}
+
+	function removeScrollListener() {
+		$(window).off("scroll");
+	}
+
+	$(window).scroll(function() {
+		triggerScrollingAnimations();
 	});
+
+	triggerScrollingAnimations();
+
 });
