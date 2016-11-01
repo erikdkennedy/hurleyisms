c6e142eb8094f31a34f3e705d0370d67bcd54ea4
Andrew Johnson <LimeyJohnson@gmail.com>
2016-11-01 05:47:31 -0700

    Changed how logging works

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(expressWinston.logger({
      })
     ],
     meta: false, // optional: control whether you want to log the meta data about the request (default to true) 
-    msg: "HTTP {{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}" 
+    msg: "HTTP StatusCode={{res.statusCode}} Method={{req.method}} {{res.responseTime}}ms URL={{req.url}} LoggedIn={{req.payload != null}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}" 
     expressFormat: false, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true 
     colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red). 
     ignoreRoute: function (req, res) { return filterLog(req); } // optional: allows to skip some log messages based on request and/or response 
